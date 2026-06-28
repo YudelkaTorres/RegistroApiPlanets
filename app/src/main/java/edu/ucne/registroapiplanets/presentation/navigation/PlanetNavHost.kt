e8748d5b9d3d593fef5f259ac31eadbd13b5c336
Yudelka Torres Amparo <yudelka_torres@ucne.edu.do>
2026-06-28 15:02:14 -0400

    Actualiza navegación para personajes.

--- app/src/main/java/edu/ucne/registroapiplanets/presentation/navigation/PlanetNavHost.kt
+++ app/src/main/java/edu/ucne/registroapiplanets/presentation/navigation/PlanetNavHost.kt
@@ -1,12 +1,18 @@
 package edu.ucne.registroapiplanets.presentation.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.compose.runtime.getValue
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import edu.ucne.registroapiplanets.presentation.detail.PlanetDetailScreen
-import edu.ucne.registroapiplanets.presentation.list.PlanetListScreen
+import edu.ucne.registroapiplanets.presentation.detail.detailPlanet.PlanetDetailScreen
+import edu.ucne.registroapiplanets.presentation.list.planetList.PlanetListScreen
+import edu.ucne.registroapiplanets.presentation.list.characterList.CharacterListScreen
+import edu.ucne.registroapiplanets.presentation.detail.detailCharacter.CharacterDetailScreen
+import edu.ucne.registroapiplanets.presentation.detail.detailCharacter.CharacterDetailViewModel
 
 @Composable
 fun PlanetNavHost(
@@ -14,7 +20,7 @@ fun PlanetNavHost(
 ) {
     NavHost(
         navController = navController,
-        startDestination = Screen.PlanetList
+        startDestination = Screen.CharacterList
     ) {
         composable<Screen.PlanetList> {
             PlanetListScreen(
@@ -25,6 +31,26 @@ fun PlanetNavHost(
         }
         composable<Screen.PlanetDetail> {
             PlanetDetailScreen(
+                onBack = {
+                    navController.navigateUp()
+                }
+            )
+        }
+
+        composable<Screen.CharacterList> {
+            CharacterListScreen(
+                onCharacterClick = { characterId ->
+                    navController.navigate(Screen.CharacterDetail(id = characterId))
+                }
+            )
+        }
+
+        composable<Screen.CharacterDetail> {
+            val viewModel: CharacterDetailViewModel = hiltViewModel()
+            val state by viewModel.state.collectAsStateWithLifecycle()
+
+            CharacterDetailScreen(
+                state = state,
                 onBack = {
                     navController.navigateUp()
                 }
