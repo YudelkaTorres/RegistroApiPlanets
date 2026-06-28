edf646fd3e800984874e4446c5b8af38bf4c4db0
Yudelka Torres Amparo <yudelka_torres@ucne.edu.do>
2026-06-28 15:00:03 -0400

    Implementa usecase de lista.

---
+++ app/src/main/java/edu/ucne/registroapiplanets/domain/usecase/GetCharactersUseCase.kt
@@ -0,0 +1,16 @@
+package edu.ucne.registroapiplanets.domain.usecase
+
+import edu.ucne.registroapiplanets.domain.repository.CharacterRepository
+import javax.inject.Inject
+
+class GetCharactersUseCase @Inject constructor(
+    private val repository: CharacterRepository
+) {
+    operator fun invoke(
+        page: Int = 1,
+        limit: Int = 10,
+        name: String? = null,
+        gender: String? = null,
+        race: String? = null
+    ) = repository.getCharacters(page, limit, name, gender, race)
+}
